73174b81e11abb1454188d8153c5d9061afc810f
Eren Türkay <turkay.eren@gmail.com>
2014-05-16 22:02:18 +0300

    [platform][realview-pb] add GIC and TIMER register definitions

--- platform/realview-pb/include/platform/realview-pb.h
+++ platform/realview-pb/include/platform/realview-pb.h
@@ -40,4 +40,47 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 #define UART0_RX_FIFO_EMPTY (1<<4)
 #define UART0_CLEAR_TO_SEND (1<<0)
 
+/*
+ * Generic Interrupt Controller registers
+ */
+
+#define GIC0  (0x1e000000)
+
+/* Main CPU registers */
+#define CONTROL  (0x00)
+#define PRIMASK  (0x04)
+#define BINPOINT (0x08)
+#define INTACK   (0x0c)
+#define EOI      (0x10)
+#define RUNNING  (0x14)
+#define HIGHPEND (0x18)
+
+/*
+ * Distribution registers
+ *
+ * Registers apart from DISTCONTROL are offsets. They are prefixed with
+ * distcontrol to be easily accessed with GICREG macro defined in interrupt.c.
+ *
+ */
+#define DISTCONTROL (0x1000)
+#define SETENABLE0  (0x1100)
+#define SETENABLE1  (0x1104)
+#define SETENABLE2  (0x1108)
+#define CLRENABLE0  (0x1180)
+#define CLRENABLE1  (0x1184)
+#define CLRENABLE2  (0x1188)
+#define SETPEND0    (0x1200)
+#define SETPEND1    (0x1204)
+#define SETPEND2    (0x1208)
+#define CLRPEND0    (0x1280)
+#define CLRPEND1    (0x1284)
+#define CLRPEND2    (0x1288)
+#define ACTIVE0     (0x1300)
+#define ACTIVE1     (0x1304)
+#define ACTIVE2     (0x1308)
+
+/* Interrupt numbers */
+#define TIMER01_INT 36
+
+
 #endif /* PLATFORM_H */
